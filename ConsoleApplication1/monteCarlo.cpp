154ade35d0addf2d300bf4afde10622205827db7
mscieszka <scieszka.marcin@pm.me>
2025-04-12 19:05:32 +0200

    fix reduction operator, improve formatting, small tweaks, ignore idea files

--- ConsoleApplication1/monteCarlo.cpp
+++ ConsoleApplication1/monteCarlo.cpp
@@ -4,14 +4,13 @@
 #include <omp.h>
 
 int main() {
-
     std::ifstream file("wynik.txt");
     if (!file.is_open()) {
-        std::cerr << "Nie mo¿na otworzyæ pliku do odczytu." << std::endl;
+        std::cerr << "Nie moÅ¼na otworzyÄ‡ pliku do odczytu." << std::endl;
         return 1;
     }
 
-    std::vector<std::pair<double, double>> points;
+    std::vector<std::pair<double, double> > points;
     double x, y;
     while (file >> x >> y) {
         points.emplace_back(x, y);
@@ -23,16 +22,20 @@ int main() {
 
     double start = omp_get_wtime();
 
-#pragma omp parallel for reduction(count++)
+#pragma omp parallel for reduction(+:count)
     for (int i = 0; i < size; i++) {
         x = points.at(i).first;
         y = points.at(i).second;
-        if (x*x+y*y <= 1.0) count++;
+        if (x * x + y * y <= 1.0) {
+            count++;
+        }
     }
 
     double end = omp_get_wtime();
-    std::cout <<  "Otrzymane przyblizenie pi: " << 4.0*count/size <<", czas wykonywania programu [ms]: " << (end-start)*1000;
 
+    double elapsed_time = (end - start) * 1000;
+    double pi_estimate = 4.0 * count / size;
+    std::cout << "Otrzymane przybliÅ¼enie pi: " << pi_estimate << ", czas wykonywania programu [ms]: " << elapsed_time << "\n";
 
     return 0;
 }
